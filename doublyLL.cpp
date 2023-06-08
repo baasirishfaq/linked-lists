a3f9204db0127e6f3cc65cd23622ed4403f7db51
baasirishfaq <baasirishfaq@gmail.com>
2023-06-08 18:25:46 +0530

    .

--- doublyLL.cpp
+++ doublyLL.cpp
@@ -3,76 +3,84 @@
 using namespace std;
 
 // making of a doubly linked list and printing elements in reverse.
- 
+
 struct Node
 {
     int data;
-    Node *next,  *prev;
+    Node *next, *prev;
 };
- 
+
 class LinkedList
 {
     Node *head, *tail;
-    public:
+
+public:
     LinkedList()
     {
         head = NULL;
         tail = NULL;
     }
-    
+
     void insertData(int value)
     {
         Node *newNode = new Node();
         newNode->prev = NULL;
         newNode->next = NULL;
         newNode->data = value;
-        
-        if(head == NULL){
+
+        if (head == NULL)
+        {
             head = tail = newNode;
-        } else {
+        }
+        else
+        {
             tail->next = newNode;
             newNode->prev = tail;
             tail = newNode;
         }
     }
-    
-    void insertAtStart(int value){
+
+    void insertAtStart(int value)
+    {
         Node *newNode = new Node();
         newNode->prev = NULL;
         newNode->next = NULL;
         newNode->data = value;
-        
-        if(head == NULL)
+
+        if (head == NULL)
         {
             head = tail = newNode;
-        } else{
+        }
+        else
+        {
             head->prev = newNode;
             newNode->next = head;
             head = newNode;
         }
     }
-    
+
     void DisplayData()
     {
         Node *temp = head;
         while (temp != NULL)
         {
-            cout<<temp->data <<"\t";
+            cout << temp->data << "\t";
             temp = temp->next;
         }
     }
-    
-    void DisplayDataReverse(){
+
+    void DisplayDataReverse()
+    {
         Node *temp = tail;
         while (temp != NULL)
         {
-            cout<<temp->data <<"\t";
+            cout << temp->data << "\t";
             temp = temp->prev;
         }
     }
 };
- 
-int main() 
+
+int main()
 {
     LinkedList obj;
     obj.insertData(10);
