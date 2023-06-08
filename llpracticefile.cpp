4b885a0e6b6c736b3847fecbbff65b4cf5821985
baasirishfaq <baasirishfaq@gmail.com>
2023-06-08 17:58:18 +0530

    new made

---
+++ llpracticefile.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+};
+
+class LinkedList
+{
+    Node *head;
+
+public:
+    LinkedList()
+    {
+        head = NULL;
+    }
+
+    void insertdata(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if (head == NULL)
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node *temp = head;
+            while (temp->next != NULL)
+            {
+                temp = temp->next;
+            }
+        }
+    }
+};
