0833e8649a4b710215ade122c613ec5f234b466e
baasirishfaq <baasirishfaq@gmail.com>
2023-06-14 23:58:33 +0530

    .

--- practicefile.cpp
+++ practicefile.cpp
@@ -1,58 +1,30 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
+#include <algorithm>
+#include <cassert>
+
 using namespace std;
-
-struct Node
-{
-    int data;
-    Node *next;
-};
-
-class LinkedList
-{
-    Node *head;
-
-public:
-    LinkedList()
-    {
-        head = NULL;
-    }
-
-    void insertdata(int value)
-    {
-        Node *newNode = new Node();
-        newNode->data = value;
-        newNode->next = NULL;
-
-        if (head == NULL)
-        {
-            head = newNode;
-        }
-        else
-        {
-            Node *temp = head;
-            while (temp->next != NULL)
-            {
-                temp = temp->next;
-            }
-            temp->next = newNode;
-        }
-    }
-
-    void displaydata()
-    {
-        Node *temp = head;
-        while (temp != NULL)
-        {
-            cout << temp->data << endl;
-            temp = temp->next;
-        }
-    }
-};
 
 int main()
 {
-    LinkedList obj;
-    obj.insertdata(10);
-    obj.insertdata(20);
-    obj.displaydata();
+    int scores[5], sum = 0;
+    {
+        cout << "enter marks for 5 subs :";
+
+        for (int i = 0; i < 5; i++)
+        {
+            cin >> scores[i];
+        }
+    }
+
+    int calculateTotalScore()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            sum = sum + scores[i];
+        }
+        cout << sum;
+    }
 }
